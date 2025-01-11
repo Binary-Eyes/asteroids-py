51c3d9da0a0e7494490ddfe7a97d2148926ca11a
amir_barak <binary.eyes.studio@gmail.com>
2025-01-11 18:36:01 +0200

    game is drawing the player triangle

--- circleshape.py
+++ circleshape.py
@@ -1,15 +1,14 @@
-from pygame.sprite import Sprite
-from pygame import Vector2
+import pygame
 
-class CircleShape(Sprite):
+class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
         if hasattr(self, "container"):
             super().__init__(self.containers)
         else:
             super().__init__()
         
-        self.position = Vector2(x, y)
-        self.velocity = Vector2(0, 0)
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
     def draw(self, screen):
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
+from constants import *
 from pygame import display
 from pygame import time
-from constants import *
+from player import Player
 
 def main():
     print('Starting asteroids!')
@@ -11,12 +12,14 @@ def main():
     deltaTime = 0.0
     gameClock = time.Clock();
     screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill((0,0,0))
+        player.draw(screen)
         display.flip()
         deltaTime = gameClock.tick(60.0)/1000.0
 
